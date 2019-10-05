48cc757b3488662d03082dea872778b4f272d8e6
bobkrstic <slobodankrsticnis@yahoo.com>
2019-10-04 18:16:03 -0700

    added small change

--- swap.c
+++ swap.c
@@ -3,6 +3,8 @@ int main(void)
 {
     int x = 28;
     int y = 50;
+    int g = 50;
+    int b = 60;
 
     printf("x is %i\n", x);
     printf("y is %i\n", y);
@@ -11,6 +13,7 @@ int main(void)
     int temp = x;
     x = y;
     y = temp;
+    g = b;
 
     printf("x is now %i\n", x);
     printf("y is now %i\n", y);
